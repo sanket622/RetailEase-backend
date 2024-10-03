c2aaac0fd7ea24ed6e7279f42790be84f6bc6a60
sanket622 <sanketkumar0068@gmail.com>
2024-10-03 20:43:28 +0530

    Update

--- db/config.js
+++ db/config.js
@@ -1,22 +1,26 @@
+// IMPORTING PACKAGE
 const mongoose = require("mongoose");
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    poolSize: 10,  // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000,  // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000,  // Close sockets after 45 seconds of inactivity
-};
 
-// Establish connection
-mongoose.connect('mongodb+srv://user121:1234@cluster0.rr2mpun.mongodb.net/RetailEase?retryWrites=true&w=majority&appName=Cluster0', options)
-    .then(() => console.log('Connection successful!'))
-    .catch(err => console.log('No connection:', err));
+// MAKING CONNECTION 
+const DB = mongoose.connect('mongodb+srv://user121:1234@cluster0.rr2mpun.mongodb.net/RetailEase?retryWrites=true&w=majority&appName=Cluster0');
 
+// Handle connection status
+DB.then(() => {
+    console.log('Connection successful!');
+}).catch((err) => {
+    console.log('No connection:', err);
+});
+
+
+// Setting it to db
 const db = mongoose.connection;
 
-// Connection event handlers
+
+// CHECKING CONNECTION
+//if error occurs
 db.on("error", console.error.bind(console, "Error connecting to DB"));
-db.once("open", function () {
+// when db connects successfully
+db.once("open", function(){
     console.log("Successfully connected to DB");
 });
